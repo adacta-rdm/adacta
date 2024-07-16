3a1916e5d4b8fc6e5a950fd7a6fd84ae61c8e32c
Johannes Riedel <62213004+joeried@users.noreply.github.com>
2024-07-16 15:48:46 +0200

    Special handling for certain specification names (#4)
    Co-authored-by: Johannes Riedel <johannes.riedel@kit.edu>
    Co-authored-by: Hendrik Gossler <h.gossler@omegadot.software>

---
+++ drizzle/migrations/0003_flowery_rhodey_repo.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "SQL_SCHEMA_PLACEHOLDER"."DeviceDefinitionSpecification" ALTER COLUMN "value" SET DATA TYPE varchar(2000);
+--> statement-breakpoint
+ALTER TABLE "SQL_SCHEMA_PLACEHOLDER"."DeviceSpecification" ALTER COLUMN "value" SET DATA TYPE varchar(2000);
+--> statement-breakpoint
+ALTER TABLE "SQL_SCHEMA_PLACEHOLDER"."SampleSpecification" ALTER COLUMN "value" SET DATA TYPE varchar(2000);
