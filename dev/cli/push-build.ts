01e3104a07943128e14fe8e8cfdf974a88c2ccc8
Johannes Riedel <62213004+joeried@users.noreply.github.com>
2024-09-06 09:00:12 +0200

    Support staging deployments using push-build (#25)
    Add support for push-build.ts to push directly to the auto-deploy branch
    for staging.
    
    ---------
    
    Co-authored-by: Johannes Riedel <johannes.riedel@kit.edu>

--- dev/cli/push-build.ts
+++ dev/cli/push-build.ts
@@ -2,9 +2,31 @@ import { shSync as sh } from "../sh";
 
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-	process.stderr.write(`Usage ${process.argv[1]} [app-name] [...paths]`);
+if (args.length < 1) {
+	process.stderr.write(
+		`Usage ${process.argv[1]} [--deploy-staging] [--origin GIT-ORIGIN] [...paths]`
+	);
 	process.exit(1);
+}
+
+const commitPaths = [];
+let deployStaging = false;
+let origin = "origin";
+
+for (let i = 0; i < args.length; i++) {
+	const arg = args[i];
+	if (arg === "-s" || arg === "--deploy-staging") {
+		deployStaging = true;
+		continue;
+	}
+
+	if (arg === "-o" || arg === "--origin") {
+		origin = args[i + 1];
+		i++;
+		continue;
+	}
+
+	commitPaths.push(arg);
 }
 
 // The name of the current branch
@@ -16,9 +38,7 @@ if (branch.startsWith("build/")) {
 	process.exit(1);
 }
 
-const commitPaths = args;
-// const appPath = join(__dirname, "../../apps", appName);
-const buildBranch = `build/${branch}`;
+const buildBranch = !deployStaging ? `build/${branch}` : `build/staging`;
 
 try {
 	// Unstage everything so only the built files are added to the commit
@@ -31,9 +51,15 @@ try {
 	// Add the built files even though they are in the .gitignore
 	sh(`git add -f ${commitPaths.join(" ")}`);
 
-	sh(`git commit -m "$(date +%F) build $(git rev-parse --short HEAD)"`);
+	const commit = sh("git rev-parse --short HEAD").trim();
+	sh(`git commit -m "$(date +%F) build ${commit}"`);
 
-	sh(`git push -f origin ${buildBranch}`);
+	sh(`git push -f ${origin} ${buildBranch}`);
+
+	if (deployStaging) {
+		// eslint-disable-next-line no-console
+		console.log(`Commit ${commit} deployed to staging`);
+	}
 } finally {
 	// Checkout the original branch again
 	sh(`git checkout ${branch}`);
