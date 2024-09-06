697f2c6f16bb2284e42d489a179f7121f4c4bc59
Johannes Riedel <62213004+joeried@users.noreply.github.com>
2024-09-06 15:11:40 +0200

    Use DI for injection of TaskDispatcher

--- apps/repo-server/src/services/ImagePreparation.ts
+++ apps/repo-server/src/services/ImagePreparation.ts
@@ -9,20 +9,18 @@ import { TaskDispatcher } from "~/apps/repo-server/src/services/TaskDispatcher/T
 import type { StorageEngineRemoteAccess } from "~/apps/repo-server/src/storage/storageEngine/remoteAccess/StorageEngineRemoteAccess";
 import type { DrizzleEntity } from "~/drizzle/DrizzleSchema";
 import type { IResourceId } from "~/lib/database/Ids";
-import { Service, ServiceContainer } from "~/lib/serviceContainer/ServiceContainer";
+import { Service } from "~/lib/serviceContainer/ServiceContainer";
 import type { StorageEngine } from "~/lib/storage-engine";
 
 /**
  * This service is responsible for preparing images for display.
  * It will take an image resource and does the following:
- * 	- Resize the image to the desired size
- * 	- Convert the image to suitable format
- * 	- If necessary the image is rotated to the correct orientation (provided by the EXIF data)
+ *    - Resize the image to the desired size
+ *    - Convert the image to suitable format
+ *    - If necessary the image is rotated to the correct orientation (provided by the EXIF data)
  */
-@Service()
+@Service(TaskDispatcher)
 export class ImagePreparation {
-	private map = new Set<string>();
-
 	/**
 	 * Filetypes supported by sharp
 	 * @private
@@ -39,7 +37,6 @@ export class ImagePreparation {
 		tif: "image/tiff",
 		webp: "image/webp",
 	};
-
 	/**
 	 * Filetypes with custom handling
 	 * @private
@@ -49,17 +46,51 @@ export class ImagePreparation {
 		heif: "image/heif",
 		bmp: "image/bmp",
 	};
-
 	private static supportedMimeTypes: string[] = Object.values({
 		...ImagePreparation.sharpExtension,
 		...ImagePreparation.customExtensions,
 	});
-
 	private static IMAGE_SIZES = {
 		icon: { width: 25, height: 25 },
 		thumbnail: { width: 200, height: 200 },
 		regular: { width: 1920, height: 1080 },
 	};
+	private map = new Set<string>();
+
+	constructor(private td: TaskDispatcher) {}
+
+	public static isSupportedMimeType(mime: string) {
+		return ImagePreparation.supportedMimeTypes.includes(mime);
+	}
+
+	private static isPictureType(type: string) {
+		const t = type.toLowerCase();
+		return t === "jpg" || t === "jpeg" || t === "heic";
+	}
+
+	private static getOptions(preset: IImagePreset, extension: string): IImageOptions {
+		switch (preset) {
+			case IImagePreset.Icon:
+				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["icon"] };
+			case IImagePreset.Thumbnail:
+				if (ImagePreparation.isPictureType(extension))
+					return { type: "jpg", maxDimensions: ImagePreparation.IMAGE_SIZES["thumbnail"] };
+				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["thumbnail"] };
+			case IImagePreset.Regular:
+				if (ImagePreparation.isPictureType(extension))
+					return { type: "jpg", maxDimensions: ImagePreparation.IMAGE_SIZES["regular"] };
+				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["regular"] };
+			default:
+				assertUnreachable(preset);
+		}
+	}
+
+	private static getPath(resourceId: IResourceId, options: IImageOptions) {
+		const sizeString = `${options.maxDimensions.height}x${options.maxDimensions.width}`;
+		const typeString = options.type;
+
+		return `images/${resourceId}_${sizeString}.${typeString}`;
+	}
 
 	/**
 	 * Prepare an image for display (@see {@link ImagePreparation})
@@ -99,7 +130,7 @@ export class ImagePreparation {
 		const downloadLink = await stoRemote.getDownloadLink(resource.id);
 		const uploadLink = await stoRemote.getUploadLink(imagePath);
 
-		await ServiceContainer.get(TaskDispatcher).dispatch("images/prepare", {
+		await this.td.dispatch("images/prepare", {
 			input: {
 				inputDownloadURL: downloadLink,
 				resultUploadURL: uploadLink,
@@ -111,38 +142,5 @@ export class ImagePreparation {
 		this.map.add(imagePath);
 
 		return getImageLink(imagePath);
-	}
-
-	private static isPictureType(type: string) {
-		const t = type.toLowerCase();
-		return t === "jpg" || t === "jpeg" || t === "heic";
-	}
-
-	public static isSupportedMimeType(mime: string) {
-		return ImagePreparation.supportedMimeTypes.includes(mime);
-	}
-
-	private static getOptions(preset: IImagePreset, extension: string): IImageOptions {
-		switch (preset) {
-			case IImagePreset.Icon:
-				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["icon"] };
-			case IImagePreset.Thumbnail:
-				if (ImagePreparation.isPictureType(extension))
-					return { type: "jpg", maxDimensions: ImagePreparation.IMAGE_SIZES["thumbnail"] };
-				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["thumbnail"] };
-			case IImagePreset.Regular:
-				if (ImagePreparation.isPictureType(extension))
-					return { type: "jpg", maxDimensions: ImagePreparation.IMAGE_SIZES["regular"] };
-				return { type: "png", maxDimensions: ImagePreparation.IMAGE_SIZES["regular"] };
-			default:
-				assertUnreachable(preset);
-		}
-	}
-
-	private static getPath(resourceId: IResourceId, options: IImageOptions) {
-		const sizeString = `${options.maxDimensions.height}x${options.maxDimensions.width}`;
-		const typeString = options.type;
-
-		return `images/${resourceId}_${sizeString}.${typeString}`;
 	}
 }
