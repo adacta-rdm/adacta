53698b51c7e503067369abe2e9b102af46cf2219
Johannes Riedel <62213004+joeried@users.noreply.github.com>
2024-11-27 15:13:01 +0100

    Avoid using axios in the frontend

--- apps/desktop-app/src/utils/uploadFileBrowser.ts
+++ apps/desktop-app/src/utils/uploadFileBrowser.ts
@@ -1,47 +1,13 @@
-import type { AxiosRequestConfig } from "axios";
-import axios from "axios";
+export async function uploadFileBrowser(file: File, uploadURL: string): Promise<void> {
+	const headers = { "Content-Type": "application/octet-stream" };
 
-import { isIUploadResourceAttachmentResponse } from "@/tsrc/lib/interface/IUploadResourceAttachmentResponse";
-import { readFile } from "~/apps/desktop-app/src/utils/readFile";
-import { assertDefined } from "~/lib/assert/assertDefined";
-import { calculateResourceAttachmentHashBrowser } from "~/lib/resources/calculateResourceAttachmentHashBrowser";
-
-export async function uploadFileBrowser(file: File, uploadURL: string): Promise<string | void> {
-	const s3Mode = uploadURL.includes("s3");
-	const formData = new FormData();
-
-	const data = await readFile(file);
-
-	const hash = await calculateResourceAttachmentHashBrowser(new Uint8Array(data));
-
-	formData.append("hashType", hash.type);
-	formData.append("hashValue", hash.value);
-	formData.append("resource", file);
-
-	const headers = s3Mode
-		? { "Content-Type": "application/octet-stream" }
-		: { "Content-Type": "multipart/form-data" };
-
-	const requestConfig: AxiosRequestConfig = {
-		maxBodyLength: 100e6,
+	const response = await fetch(uploadURL, {
+		method: "PUT",
 		headers,
-	};
+		body: file,
+	});
 
-	const response = s3Mode
-		? await axios.put(uploadURL, file, requestConfig)
-		: await axios.post(uploadURL, formData, requestConfig);
-
-	if (!s3Mode) {
-		if (response.status !== 200 || !isIUploadResourceAttachmentResponse(response.data)) {
-			throw new Error("Unexpected answer for HTTP Request");
-		}
-
-		if (!response.data.result) {
-			throw new Error(response.data.message);
-		}
-
-		assertDefined(response.data.uploadId);
-
-		return response.data.uploadId;
+	if (response.status !== 200) {
+		throw new Error("Unexpected answer for HTTP Request");
 	}
 }
