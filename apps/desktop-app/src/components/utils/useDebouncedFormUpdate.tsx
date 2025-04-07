adacta-rdm/adacta

commit 2d10aa7ec3236321690fc3ace5690d3377e57dab

ResourceComparison: Avoid stale data + Add sharable URL (#33)
Co-authored-by: Hendrik Gossler <h.gossler@omegadot.software>

apps/desktop-app/src/components/utils/useDebouncedFormUpdate.tsx
@@ -1,22 +1,26 @@
 import { useDebounceCallback } from "@react-hook/debounce";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import type { Primitive } from "type-fest";
 
 /**
  * Debounces the calls to the updaterFunction while instantly updating the (returned) value
  */
-export function useDebounceFormUpdate<T>(
+export function useDebounceFormUpdate<T extends Primitive | Primitive[]>(
 	initialValue: T,
 	updateFunction: (v: T) => void,
 	delay: number
-): [T, (e: T) => void] {
+): [T, (e: T | ((e: T) => T)) => void] {
 	const onChangeDebounced = useDebounceCallback(updateFunction, delay);
 
-	const onChange = (e: T) => {
-		setValue(e);
-		onChangeDebounced(e);
-	};
-
 	const [value, setValue] = useState<T>(initialValue);
+
+	const onChange = useCallback(
+		() => (e: T | ((e: T) => T)) => {
+			setValue(e);
+			onChangeDebounced(typeof e === "function" ? e(value) : e);
+		},
+		[onChangeDebounced, value]
+	);
 
 	// Update the value if the initialValue changes (usually caused by the execution of
 	// updaterFunction)

apps/desktop-app/src/components/chart/Chart.tsx
@@ -10,6 +10,7 @@ import {
 } from "@elastic/charts";
 import { EuiFlexGroup, EuiFlexItem, EuiPanel } from "@elastic/eui";
 import { assertDefined } from "@omegadot/assert";
+import { merge } from "lodash";
 import React, { Fragment, Suspense } from "react";
 import { graphql } from "react-relay";
 import { useFragment } from "react-relay/hooks";
@@ -32,9 +33,19 @@ interface IProps {
 	hideDevices?: string[];
 
 	/**
-	 * Label of the series which should be highlighted (all other series should get a lighter color)
+	 * Select a series which should be highlighted (all other series should get a lighter color)
 	 */
-	highlightSeries?: string;
+	highlight?: {
+		/**
+		 * Name of the series
+		 */
+		name?: string;
+
+		/**
+		 * Resource ID of the series
+		 */
+		resourceId?: string;
+	};
 }
 
 const ChartGraphQLFragment = graphql`
@@ -96,13 +107,20 @@ export function Chart(props: IProps) {
 
 	const getLineStyleByIndex = (
 		index: number,
-		bold?: boolean
+		highlight?: boolean
 	): RecursivePartial<LineSeriesStyle> => {
-		if (index === 0) {
-			return {
-				line: {
-					strokeWidth: bold ? 2 : 1,
-				},
+		const baseConfig: RecursivePartial<LineSeriesStyle> = {
+			// point: resources.length > 1 ? { visible: false } : undefined, // Hide points in comparison charts
+			point: { visible: false },
+			line: {
+				strokeWidth: highlight ? 2 : 1,
+			},
+		};
+
+		// Use the default line style (solid line) for the first resource and for highlighted resources
+		// This ensures that the increased stroke width is easier noticeable
+		if (index === 0 || highlight) {
+			return merge(baseConfig, {
 				fit: {
 					line: {
 						// EUI draws lines to data points that were filled by the "fit" function
@@ -112,8 +130,9 @@ export function Chart(props: IProps) {
 						dash: [Infinity],
 					},
 				},
-			};
+			});
 		} else {
+			// Syntax is [dashLength, gapLength, dashLength, gapLength, ...]
 			const dashConfigs = [
 				[1], // Dots
 				[5], // Dashes
@@ -124,7 +143,7 @@ export function Chart(props: IProps) {
 			const dashConfigIndex = (index - 1) % dashConfigs.length;
 			const dashConfig = dashConfigs[dashConfigIndex];
 
-			return {
+			return merge(baseConfig, {
 				line: {
 					dash: dashConfig,
 				},
@@ -133,12 +152,16 @@ export function Chart(props: IProps) {
 						dash: dashConfig,
 					},
 				},
-			};
+			});
 		}
 	};
 
-	const getColor = (axisTitle: string, axisLabel: string) => {
-		if (props.highlightSeries !== undefined && props.highlightSeries !== axisLabel) {
+	const getColor = (resourceId: string | null, axisTitle: string, axisLabel: string) => {
+		if (
+			props.highlight !== undefined &&
+			props.highlight.name !== axisLabel &&
+			props.highlight.resourceId !== resourceId
+		) {
 			return colorAssigner.getLightColor(axisTitle);
 		}
 
@@ -151,7 +174,7 @@ export function Chart(props: IProps) {
 		const axisId = `${axisTitle}${i}`;
 
 		// Request color even for hidden axes to get consistent colors
-		const color = getColor(axisTitle, yAxis.label);
+		const color = getColor(yAxis.resourceId, axisTitle, yAxis.label);
 
 		if (yAxis.device?.id !== null && props.hideDevices?.includes(yAxis.device?.id as IDeviceId)) {
 			return [];
@@ -192,7 +215,9 @@ export function Chart(props: IProps) {
 							xScaleType={xValueIsTime ? ScaleType.Time : ScaleType.Linear}
 							lineSeriesStyle={getLineStyleByIndex(
 								getResourceIndex(yAxis.resourceId),
-								props.highlightSeries !== undefined && props.highlightSeries === yAxis.label
+								props.highlight !== undefined &&
+									(props.highlight.name === yAxis.label ||
+										props.highlight.resourceId === yAxis.resourceId)
 							)}
 						/>
 					);

apps/desktop-app/src/components/chart/MultipleResourceChart.tsx
@@ -11,6 +11,9 @@ import { ChartLoading } from "~/apps/desktop-app/src/components/chart/ChartLoadi
 
 interface IProps {
 	resourceIds: string[];
+
+	highlightResourceId?: string;
+
 	offsets?: number[];
 
 	alignStart?: boolean;
@@ -79,7 +82,13 @@ export function MultipleResourceChart(props: IProps) {
 	return (
 		<>
 			{data.mergedResourceChart.map((data, i) => (
-				<Chart chart={data} key={i} />
+				<Chart
+					chart={data}
+					key={JSON.stringify({ ...props, key: i })}
+					highlight={
+						props.highlightResourceId ? { resourceId: props.highlightResourceId } : undefined
+					}
+				/>
 			))}
 		</>
 	);

apps/desktop-app/src/components/chart/ResourceChart.tsx
@@ -173,7 +173,7 @@ export function ResourceChart(props: IProps) {
 		<>
 			<Chart
 				hideDevices={props.hideDevices}
-				highlightSeries={highlightSeries}
+				highlight={{ name: highlightSeries }}
 				chart={data.resource.downSampled}
 				customLegend={
 					<>

apps/desktop-app/src/components/chart/Sparkline.tsx
@@ -70,7 +70,7 @@ export function Sparklines(props: { resource: Sparkline$key }) {
 
 	return (
 		<EuiStat title="" description="" textAlign="right">
-			<Chart size={{ height: 100, width: 300 }}>
+			<Chart size={{ height: 100, width: 200 }}>
 				<Settings theme={theme} showLegend={false} />
 				{dataSlices.map((slice, i) => (
 					<LineSeries

apps/desktop-app/src/components/resource/ResourceComparisonView.tsx
@@ -1,105 +1,0 @@
-import {
-	EuiButton,
-	EuiFlexGroup,
-	EuiFlexItem,
-	EuiSpacer,
-	EuiSwitch,
-	EuiToolTip,
-} from "@elastic/eui";
-import { assertDefined } from "@omegadot/assert";
-import React, { Suspense, useState } from "react";
-import { graphql, useLazyLoadQuery } from "react-relay";
-
-import { MultiTimeSelector } from "./MultiTimeSelector";
-import { ResourceLink } from "./ResourceLink";
-import { secondsToHMS } from "../../utils/secondsToHMS";
-import { ChartLoading } from "../chart/ChartLoading";
-import { MultipleResourceChart } from "../chart/MultipleResourceChart";
-
-import type { ResourceComparisonViewQuery } from "@/relay/ResourceComparisonViewQuery.graphql";
-import { useRepositoryIdVariable } from "~/apps/desktop-app/src/services/router/UseRepoId";
-
-export function ResourceComparisonView(props: {
-	onLeaveComparisonView: () => void;
-	selectedResources: string[];
-}) {
-	const {
-		repository: { nodes: resources },
-	} = useLazyLoadQuery<ResourceComparisonViewQuery>(
-		graphql`
-			query ResourceComparisonViewQuery($repositoryId: ID!, $resourceIds: [ID!]!) {
-				repository(id: $repositoryId) {
-					nodes(ids: $resourceIds) {
-						... on ResourceTabularData {
-							id
-							...ResourceLink
-						}
-					}
-				}
-			}
-		`,
-		{ ...useRepositoryIdVariable(), resourceIds: props.selectedResources }
-	);
-	const [showComparisonAlignStart, setShowComparisonAlignStart] = useState(false);
-	const [offsets, setOffsets] = useState<number[]>(
-		new Array(props.selectedResources.length).fill(0)
-	);
-	const [offsetEditMode, setOffsetEditMode] = useState(false);
-
-	const { onLeaveComparisonView, selectedResources } = props;
-	return (
-		<>
-			<EuiFlexGroup>
-				<EuiFlexItem grow={false}>
-					<EuiButton onClick={() => onLeaveComparisonView()}>Back to resource list</EuiButton>
-				</EuiFlexItem>
-				<EuiFlexItem grow={false}>
-					<EuiButton onClick={() => setOffsetEditMode(!offsetEditMode)}>Edit offsets</EuiButton>
-				</EuiFlexItem>
-				<EuiFlexItem grow={false}>
-					<EuiToolTip
-						content={
-							"Aligns all resources to the same start time. Instead of different absolute start times, a common start point is assumed for all data series."
-						}
-					>
-						<EuiSwitch
-							label={"Align begin"}
-							checked={showComparisonAlignStart}
-							onChange={(e) => setShowComparisonAlignStart(e.target.checked)}
-						/>
-					</EuiToolTip>
-				</EuiFlexItem>
-			</EuiFlexGroup>
-			<EuiSpacer />
-			<EuiFlexGroup direction={"column"}>
-				{resources.map((r) => {
-					assertDefined(r?.id);
-					const index = selectedResources.findIndex((s) => s === r.id);
-					const [h, m, s] = secondsToHMS(offsets[index]).map((n) => String(n).padStart(2, "0"));
-					return (
-						<EuiFlexItem key={r.id}>
-							<ResourceLink resource={r} />
-							Offset: {h}:{m}:{s}
-							{offsetEditMode && (
-								<MultiTimeSelector
-									value={offsets[index]}
-									onChange={(vNew) => {
-										setOffsets((offsets) => offsets.map((vOld, i) => (i === index ? vNew : vOld)));
-									}}
-								/>
-							)}
-						</EuiFlexItem>
-					);
-				})}
-			</EuiFlexGroup>
-			<EuiSpacer />
-			<Suspense fallback={<ChartLoading />}>
-				<MultipleResourceChart
-					resourceIds={selectedResources}
-					alignStart={showComparisonAlignStart}
-					offsets={offsets.map((o) => o * 1000)}
-				/>
-			</Suspense>
-		</>
-	);
-}

apps/desktop-app/src/components/resource/ResourceList.tsx
@@ -13,8 +13,8 @@ import type { PreloadedQuery } from "react-relay/hooks";
 import { usePreloadedQuery } from "react-relay/hooks";
 import type { GraphQLSubscriptionConfig } from "relay-runtime";
 
-import { ResourceComparisonView } from "./ResourceComparisonView";
 import { ResourceListHeader, ResourceListTable } from "./list/ResourceListTable";
+import { useRouter } from "../../hooks/useRouter";
 
 import type { ResourceList$key } from "@/relay/ResourceList.graphql";
 import type { ResourceListAddedOrUpdatedSubscription } from "@/relay/ResourceListAddedOrUpdatedSubscription.graphql";
@@ -22,6 +22,7 @@ import type { ResourceListFragment } from "@/relay/ResourceListFragment.graphql"
 import type { ResourceListQuery } from "@/relay/ResourceListQuery.graphql";
 import type { ResourceListRemovedSubscription } from "@/relay/ResourceListRemovedSubscription.graphql";
 import { SearchEmptyPrompt } from "~/apps/desktop-app/src/components/search/list/SearchEmptyPrompt";
+import { useRepositoryIdVariable } from "~/apps/desktop-app/src/services/router/UseRepoId";
 
 const ResourceListGraphQLFragment = graphql`
 	fragment ResourceList on RepositoryQuery
@@ -93,6 +94,9 @@ export function ResourceList(props: {
 	);
 	props.setRefetch(() => refetch);
 
+	const { router } = useRouter();
+	const repositoryIdVariable = useRepositoryIdVariable();
+
 	const data = fullData.repository;
 	const connectionId = data.resources.__id;
 
@@ -118,20 +122,22 @@ export function ResourceList(props: {
 
 	// Comparison
 	const [selectedResources, setSelectedResources] = useState<string[]>([]);
-	const [showComparison, setShowComparison] = useState(false);
 
-	return showComparison ? (
-		<ResourceComparisonView
-			onLeaveComparisonView={() => setShowComparison(false)}
-			selectedResources={selectedResources}
-		/>
-	) : (
+	return (
 		<>
 			{selectedResources.length > 0 && (
 				<>
 					<EuiFlexGroup justifyContent={"flexEnd"}>
 						<EuiFlexItem grow={false}>
-							<EuiButton onClick={() => setShowComparison(true)}>
+							<EuiButton
+								onClick={() =>
+									router.push(
+										"/repositories/:repositoryId/resources/comparison",
+										repositoryIdVariable,
+										{ resourceIds: selectedResources }
+									)
+								}
+							>
 								Compare {selectedResources.length} resources
 							</EuiButton>
 						</EuiFlexItem>

apps/desktop-app/src/routes/repositories.$repositoryId.resources.comparison.tsx
@@ -1,0 +1,168 @@
+import {
+	EuiButton,
+	EuiFlexGroup,
+	EuiFlexItem,
+	EuiPageTemplate,
+	EuiSpacer,
+	EuiSwitch,
+	EuiToolTip,
+} from "@elastic/eui";
+import { assertDefined } from "@omegadot/assert";
+import { useDebounce, useDebounceCallback } from "@react-hook/debounce";
+import React, { Suspense, useState } from "react";
+import { graphql, loadQuery } from "react-relay";
+import { usePreloadedQuery } from "react-relay/hooks";
+
+import { ListPageLoading } from "../components/layout/ListPageLoading";
+
+import type { repositoriesRepositoryIdResourcesComparisonQuery as Query } from "@/relay/repositoriesRepositoryIdResourcesComparisonQuery.graphql";
+import type { GetDataArgs, Props } from "@/routes/repositories.$repositoryId.resources.comparison";
+import { ChartLoading } from "~/apps/desktop-app/src/components/chart/ChartLoading";
+import { MultipleResourceChart } from "~/apps/desktop-app/src/components/chart/MultipleResourceChart";
+import { AdactaPageTemplate } from "~/apps/desktop-app/src/components/layout/AdactaPageTemplate";
+import { MultiTimeSelector } from "~/apps/desktop-app/src/components/resource/MultiTimeSelector";
+import { ResourceLink } from "~/apps/desktop-app/src/components/resource/ResourceLink";
+import { useRouter } from "~/apps/desktop-app/src/hooks/useRouter";
+import { useRepositoryId } from "~/apps/desktop-app/src/services/router/UseRepoId";
+import { secondsToHMS } from "~/apps/desktop-app/src/utils/secondsToHMS";
+
+export type QueryParams = {
+	resourceIds?: string[];
+	offsets?: number[];
+	alignStart?: boolean;
+};
+
+const ResourceComparisonViewGraphqlQuery = graphql`
+	query repositoriesRepositoryIdResourcesComparisonQuery($repositoryId: ID!, $resourceIds: [ID!]!) {
+		repository(id: $repositoryId) {
+			nodes(ids: $resourceIds) {
+				... on ResourceTabularData {
+					id
+					...ResourceLink
+				}
+			}
+		}
+	}
+`;
+
+function getData({ match, relayEnvironment }: GetDataArgs) {
+	const resourceIds = match.query.resourceIds ?? [];
+
+	return loadQuery<Query>(
+		relayEnvironment,
+		ResourceComparisonViewGraphqlQuery,
+		{
+			repositoryId: match.params.repositoryId,
+			resourceIds,
+		},
+		{ fetchPolicy: "store-and-network" }
+	);
+}
+
+export default function Route(props: Props<typeof getData>) {
+	return (
+		<AdactaPageTemplate>
+			<EuiPageTemplate.Header
+				pageTitle={
+					<EuiFlexGroup alignItems="baseline" gutterSize="xs">
+						<EuiFlexItem grow={false}>Resources</EuiFlexItem>
+					</EuiFlexGroup>
+				}
+			/>
+			<EuiPageTemplate.Section>
+				<Suspense fallback={<ListPageLoading pageTitle="Resources" />}>
+					<ResourceComparisonView {...props} />
+				</Suspense>
+			</EuiPageTemplate.Section>
+		</AdactaPageTemplate>
+	);
+}
+
+export function ResourceComparisonView(props: Props<typeof getData>) {
+	const {
+		repository: { nodes: resources },
+	} = usePreloadedQuery(ResourceComparisonViewGraphqlQuery, props.data);
+	const selectedResources = props.match.query.resourceIds ?? [];
+	const showComparisonAlignStart = props.match.query.alignStart ?? false;
+	const offsets = props.match.query.offsets ?? selectedResources.map(() => 0);
+
+	const setOffsets = useDebounceCallback(
+		(offsets: number[]) => props.setQueryParam("offsets", offsets),
+		300
+	);
+
+	const [offsetEditMode, setOffsetEditMode] = useState(false);
+	const repositoryId = useRepositoryId();
+	const { router } = useRouter();
+
+	const onLeaveComparisonView = () => {
+		router.push("/repositories/:repositoryId/resources/", {
+			repositoryId,
+		});
+	};
+
+	// This state is used to highlight the resource in the chart when hovering over the resource in the list
+	// The state is debounced to prevent flickering when moving the mouse over the resource names
+	const [hoveredResource, setHoveredResource] = useDebounce<string | undefined>(undefined, 500);
+
+	return (
+		<>
+			<EuiFlexGroup>
+				<EuiFlexItem grow={false}>
+					<EuiButton onClick={() => onLeaveComparisonView()}>Back to resource list</EuiButton>
+				</EuiFlexItem>
+				<EuiFlexItem grow={false}>
+					<EuiButton onClick={() => setOffsetEditMode(!offsetEditMode)}>Edit offsets</EuiButton>
+				</EuiFlexItem>
+				<EuiFlexItem grow={false}>
+					<EuiToolTip
+						content={
+							"Aligns all resources to the same start time. Instead of different absolute start times, a common start point is assumed for all data series."
+						}
+					>
+						<EuiSwitch
+							label={"Align begin"}
+							checked={showComparisonAlignStart}
+							onChange={(e) => props.setQueryParam("alignStart", e.target.checked)}
+						/>
+					</EuiToolTip>
+				</EuiFlexItem>
+			</EuiFlexGroup>
+			<EuiSpacer />
+			<EuiFlexGroup direction={"column"}>
+				{resources.map((r) => {
+					assertDefined(r?.id);
+					const index = selectedResources.findIndex((s) => s === r.id);
+					const [h, m, s] = secondsToHMS(offsets[index]).map((n) => String(n).padStart(2, "0"));
+					return (
+						<EuiFlexItem
+							key={r.id}
+							onMouseEnter={() => setHoveredResource(r.id)}
+							onMouseLeave={() => setHoveredResource(undefined)}
+						>
+							<ResourceLink resource={r} />
+							Offset: {h}:{m}:{s}
+							{offsetEditMode && (
+								<MultiTimeSelector
+									value={offsets[index]}
+									onChange={(vNew) => {
+										setOffsets(offsets.map((vOld, i) => (i === index ? vNew : vOld)));
+									}}
+								/>
+							)}
+						</EuiFlexItem>
+					);
+				})}
+			</EuiFlexGroup>
+			<EuiSpacer />
+			<Suspense fallback={<ChartLoading />}>
+				<MultipleResourceChart
+					resourceIds={selectedResources}
+					highlightResourceId={hoveredResource}
+					alignStart={showComparisonAlignStart}
+					offsets={offsets.map((o) => o * 1000)}
+				/>
+			</Suspense>
+		</>
+	);
+}

commit 4f066c5676afcf758b696e1a2fc9990e4328cccc

Fix useDebouncedFormUpdate (#35)
Fix useDebouncedFormUpdate which was broken in #33

apps/desktop-app/src/components/utils/useDebouncedFormUpdate.tsx
@@ -15,7 +15,7 @@ export function useDebounceFormUpdate<T extends Primitive | Primitive[]>(
 	const [value, setValue] = useState<T>(initialValue);
 
 	const onChange = useCallback(
-		() => (e: T | ((e: T) => T)) => {
+		(e: T | ((e: T) => T)) => {
 			setValue(e);
 			onChangeDebounced(typeof e === "function" ? e(value) : e);
 		},

apps/desktop-app/src/stories/DebouncedFormUpdate.stories.tsx
@@ -1,0 +1,44 @@
+import { EuiFieldText } from "@elastic/eui";
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+import { useDebounceFormUpdate } from "~/apps/desktop-app/src/components/utils/useDebouncedFormUpdate";
+
+export default {
+	title: "Utils/DebouncedFormUpdate",
+	component: DebounceConsumer,
+} as Meta<typeof DebounceConsumer>;
+
+type Story = StoryObj<typeof DebounceConsumer>;
+
+export const Basic: Story = {
+	args: {
+		initialValue: "Foobar123",
+	},
+};
+
+function DebounceConsumer(props: { initialValue: string }) {
+	// This value will be updated using the updateFunction of useDebounceFormUpdate
+	const [debouncedValue, setDebouncedValue] = useState(props.initialValue);
+
+	// Actual value + onChange function with debouncing
+	const [value, onChange] = useDebounceFormUpdate<string>(
+		props.initialValue,
+		setDebouncedValue,
+		2000
+	);
+
+	return (
+		<pre>
+			Debounced Value: {debouncedValue}
+			{"\n"}
+			Value: {value}
+			<EuiFieldText
+				value={value}
+				onChange={(e) => {
+					onChange(e.target.value);
+				}}
+			/>
+		</pre>
+	);
+}